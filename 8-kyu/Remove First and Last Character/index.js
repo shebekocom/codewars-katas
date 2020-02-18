4333f6f39e08ed322237af72cbe8f0a2fa6c30b2
nshebeko <nikolay.shebeko@gmail.com>
2020-02-18 23:48:48 +0300

    feat: add solution 8 kyu: Remove First and Last Character

---
+++ 8-kyu/Remove First and Last Character/index.js
@@ -0,0 +1,5 @@
+function removeChar(str) {
+  return str.substring(1, str.length-1);
+}
+
+module.exports = removeChar;
---
+++ 8-kyu/Remove First and Last Character/index.test.js
@@ -0,0 +1,14 @@
+const removeChar = require('.');
+
+test('Remove First and Last Character', () => {
+  expect(removeChar('eloquent')).toBe('loquen');
+});
+test('Remove First and Last Character', () => {
+  expect(removeChar('country')).toBe('ountr');
+});
+test('Remove First and Last Character', () => {
+  expect(removeChar('person')).toBe('erso');
+});
+test('Remove First and Last Character', () => {
+  expect(removeChar('place')).toBe('lac');
+});
