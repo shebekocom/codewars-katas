49cdb53408679460e9f396f75ed860678df9e12c
nshebeko <nikolay.shebeko@gmail.com>
2020-03-03 00:57:48 +0300

    fix: text error in test

--- 7-kyu/Fizz Buzz/index.test.js
+++ 7-kyu/Fizz Buzz/index.test.js
@@ -1,6 +1,6 @@
 const fizzbuzz = require('.');
 
-describe('Sum of positive', () => {
+describe('Fizz Buzz', () => {
   test('Test 1', () => {
     const expected = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz'];
     expect(fizzbuzz(10)).toEqual(expected);
